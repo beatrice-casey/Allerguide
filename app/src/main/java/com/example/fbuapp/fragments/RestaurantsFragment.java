892f12f660b759f2713f53158d73aa8bb1d55eb7
Beatrice Casey <bac041@bucknell.edu>
2020-07-13 15:12:59 -0400

    Created bottom tab navigation, recycler view and individual restaurant view.

---
+++ app/src/main/java/com/example/fbuapp/fragments/RestaurantsFragment.java
@@ -0,0 +1,12 @@
+package com.example.fbuapp.fragments;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RestaurantsFragment extends Fragment {
+
+    public static final String TAG = "RestaurantsFragment";
+    protected RecyclerView rvRestaurants;
+
+
+}
