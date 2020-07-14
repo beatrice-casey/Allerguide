f4f1a932bf790c2b42c846e64f16d55dd7b1a6d6
Beatrice Casey <bac041@bucknell.edu>
2020-07-14 16:36:34 -0400

    updated location settings

--- app/src/main/java/com/example/fbuapp/activities/MainActivity.java
+++ app/src/main/java/com/example/fbuapp/activities/MainActivity.java
@@ -27,13 +27,10 @@ import static com.google.android.gms.location.LocationServices.getFusedLocationP
 
 public class MainActivity extends AppCompatActivity {
 
-    private LocationRequest mLocationRequest;
+
     public static final String TAG = "MainActivity";
 
-    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
-    private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
-    public Location location;
 
 
     private BottomNavigationView bottomNavigationView;
@@ -42,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startLocationUpdates();
         //Log.i(TAG, "google maps URL: " + LOCATION_URL);
 
 
@@ -88,48 +84,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    // Trigger new location updates at interval
-    protected void startLocationUpdates() {
-
-        // Create the location request to start receiving updates
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(UPDATE_INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-
-        // Create LocationSettingsRequest object using location request
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        builder.addLocationRequest(mLocationRequest);
-        LocationSettingsRequest locationSettingsRequest = builder.build();
-
-        // Check whether location settings are satisfied
-        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
-        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
-        settingsClient.checkLocationSettings(locationSettingsRequest);
-
-        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new
-                        LocationCallback() {
-                            @Override
-                            public void onLocationResult(LocationResult locationResult) {
-                                // do work here
-                                //TODO: use this location to find restaurants near by
-                                location = locationResult.getLastLocation();
-
-                            }
-                        },
-                Looper.myLooper());
-    }
 
 }
--- app/src/main/java/com/example/fbuapp/fragments/RestaurantsFragment.java
+++ app/src/main/java/com/example/fbuapp/fragments/RestaurantsFragment.java
@@ -1,6 +1,10 @@
 package com.example.fbuapp.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +12,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +23,12 @@ import com.example.fbuapp.R;
 import com.example.fbuapp.Restaurant;
 import com.example.fbuapp.RestaurantsAdapter;
 import com.example.fbuapp.activities.MainActivity;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,6 +38,8 @@ import java.util.ArrayList;
 
 import okhttp3.Headers;
 
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
 public class RestaurantsFragment extends Fragment {
 
     public static final String TAG = "RestaurantsFragment";
@@ -35,6 +48,14 @@ public class RestaurantsFragment extends Fragment {
     protected RestaurantsAdapter adapter;
     public String LOCATION_URL;
     public String MAPS_API_KEY;
+
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
+
+    public String latitude;
+    public String longitude;
+
+    private LocationRequest mLocationRequest;
 
     public RestaurantsFragment() {
         // Required empty public constructor
@@ -51,7 +72,10 @@ public class RestaurantsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         MAPS_API_KEY = getString(R.string.google_maps_API_key);
-        LOCATION_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.357300,-74.667221&radius=1500&type=restaurant&key=" + MAPS_API_KEY;
+        startLocationUpdates();
+        LOCATION_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude
+                + "," + longitude + "&radius=1500&type=restaurant&key=" + MAPS_API_KEY;
+        Log.i(TAG, "URL: "+ LOCATION_URL);
         rvRestaurants = view.findViewById(R.id.rvRestaurants);
         restaurants = new ArrayList<>();
         adapter = new RestaurantsAdapter(getContext(), restaurants);
@@ -92,6 +116,57 @@ public class RestaurantsFragment extends Fragment {
             }
         });
 
+    }
+
+
+    // Trigger new location updates at interval
+    protected void startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(getContext());
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            latitude = "40.3173";
+            longitude = "-74.6199";
+            return;
+        }
+        getFusedLocationProviderClient(getContext()).requestLocationUpdates(mLocationRequest, new
+                        LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+                                // do work here
+                                //TODO: use this location to find restaurants near by
+                                Location location = locationResult.getLastLocation();
+                                latitude = Double.toString(location.getLatitude());
+                                longitude = Double.toString(location.getLongitude());
+
+
+                            }
+                        },
+                Looper.myLooper());
     }
 
 
