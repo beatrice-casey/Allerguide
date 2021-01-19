6072f3e90dcb878e566bfa7c7bd85bfb83f793fb
Beatrice Casey <bac041@bucknell.edu>
2021-01-19 14:20:19 -0500

    release finalized

--- app/src/test/java/com/example/fbuapp/restaurantsTest.java
+++ app/src/test/java/com/example/fbuapp/restaurantsTest.java
@@ -1,23 +1,14 @@
 package com.example.fbuapp;
 
 
-import android.app.Application;
-
-import androidx.arch.core.executor.TaskExecutor;
-import androidx.lifecycle.Observer;
-
-import com.example.fbuapp.home.MainActivity;
-import com.example.fbuapp.home.RestaurantsAdapter;
-import com.example.fbuapp.home.RestaurantsFragment;
-import com.example.fbuapp.home.RestaurantsViewModel;
-import com.example.fbuapp.models.Restaurant;
+import com.example.allerguide.home.RestaurantsAdapter;
+import com.example.allerguide.home.RestaurantsFragment;
+import com.example.allerguide.home.RestaurantsViewModel;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.junit.runners.model.TestClass;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
